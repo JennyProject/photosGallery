5aa39bd662131c5ade8a00e0513a0fa10bc05e58
jennymo <1162279101@qq.com>
2023-05-09 00:06:31 +0800

    vite配置按需加载组件，公共样式文件引入，目录别名配置

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, 'src')
-    }
-  },
+export default defineConfig(() => {
+  return{
+    plugins: [vue(),
+      AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, 'src')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          /**如果引入多个文件，可以使用
+         * '@import "@/assets/scss/globalVariable1.scss";
+         * @import"@/assets/scss/globalVariable2.scss";'
+        **/
+          additionalData: '@import "@/assets/scss/var.scss";',
+        }
+      }
+    },
+  }
 })
